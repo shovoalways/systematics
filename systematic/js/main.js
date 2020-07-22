eb5727f849629da7d64498c684288b93d5bd46f3
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2020-07-22 14:08:37 +0600

    Add files via upload

---
+++ systematic/js/main.js
@@ -0,0 +1,8 @@
+// Bx Slider
+  $(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('#main_menu').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth: 991,
+    });
+  });
